727484f584f2171dddbd368c06865154b85419b6
Mohamed Nachit <mohamed.nachit@koji.fr>
2022-03-09 23:46:09 +0100

    guzzle

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
 
 $name =  $_GET['name'] ?? 'John doe';
 $timeout = $_GET['wait'] ?? 0;
 
-sleep($timeout);
+$licenseKey = getenv("NEW_RELIC_LICENSE_KEY");
+// Send an asynchronous request.
+$client = new \GuzzleHttp\Client();
+$request = new \GuzzleHttp\Psr7\Request(
+    'POST',
+    "https://log-api.newrelic.com/log/v1?Api-Key=$licenseKey",
+    [
+        'json' => ["timestamp" => time(), "message" => "$name => $timeout"],
+        'headers' => [
+            'Content-Type' => 'application/json',
+        ]
+    ]
+);
+$promise = $client->sendAsync($request)->then(function ($response) {
+    echo 'I completed! ' . $response->getBody();
+});
+
+$promise->wait();
 
 echo "Welcome $name";
-
